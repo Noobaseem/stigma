e99a1d58f70d9cc3aceecfd1a1341f470a2207fa
Aseem <aseem@Aseems-MacBook-Pro.local>
2016-10-06 08:31:10 +0530

    Adding the seed structure of the backend in Spring boot

---
+++ logiks-prep/src/main/java/in/stigma/LogiksPrepApplication.java
@@ -0,0 +1,12 @@
+package in.stigma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogiksPrepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogiksPrepApplication.class, args);
+	}
+}
