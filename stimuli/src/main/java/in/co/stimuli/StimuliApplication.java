0d8c0ba090b8a0eb384f4310c5cb53a0bd4b17aa
Noobaseem <aseem.rastogi1992@gmail.com>
2016-12-07 19:19:31 +0530

    Adding the backend architecture for the application

---
+++ stimuli/src/main/java/in/co/stimuli/StimuliApplication.java
@@ -0,0 +1,12 @@
+package in.co.stimuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StimuliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StimuliApplication.class, args);
+	}
+}
