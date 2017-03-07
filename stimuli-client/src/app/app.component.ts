fdd9c60a6d99e4c1b31520375be42913718c9639
Noobaseem <aseem.rastogi1992@gmail.com>
2017-03-07 20:45:48 +0530

    adding few enhancements in the app

--- stimuli-client/src/app/app.component.ts
+++ stimuli-client/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { Constants } from '../utilities/constants';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app works!';
+
+	title = Constants.APP_TITLE;
+	
 }
--- stimuli-client/src/app/app.module.ts
+++ stimuli-client/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
+/*Dev Modules*/
+import { LandingModule } from '../landing/landing.module';
+import { NavbarModule } from '../navbar/navbar.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -12,7 +15,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    LandingModule,
+    NavbarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
